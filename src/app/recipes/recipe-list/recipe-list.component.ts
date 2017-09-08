022f02f8e198b484688fb71ac6e15eacda32e103
lk <lucaskeys@gmail.com>
2017-09-07 18:32:03 -0700

    add receipe list component html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[]  = [
+    new Recipe('A test recipe', 'This is a simply a test', 'http://www.ndtv.com/cooks/images/kerala.chicken.roast%20%281%29.jpg')
+  ];
+
 
   constructor() { }
 
